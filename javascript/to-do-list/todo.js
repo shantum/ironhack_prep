55b5f087aea16daf85568ac573e30567b1c23637
shantum <shantumgupta@gmail.com>
2016-10-14 21:39:04 +0200

    added todo list files

---
+++ javascript/to-do-list/todo.js
@@ -0,0 +1,27 @@
+
+window.onload = function() {
+
+// the following code adds event listeners to the buttons
+// you'll learn more about this later
+// for this exercise, you are going to write the functions for
+// what happens when the user clicks on the buttons.
+  var saveButton = document.getElementById('save-button');
+  saveButton.addEventListener('click', addToDoItem, false);
+
+  var doneButton = document.getElementById('done-button');
+  doneButton.addEventListener('click', markAsDone, false);
+
+
+  function addToDoItem() {
+  var input = document.getElementById("todo-input").value;
+  var entry = document.createElement("li")
+  var todoList = document.getElementById("todo-list-items");
+  entry.appendChild(document.createTextNode(input));
+  todoList.appendChild(entry);
+    }
+
+  function markAsDone() {
+    doneButton.classList.add('liked');
+    doneButton.innerHTML = "Liked!";
+    document.querySelector('h1').style.color = "red";
+  }}
