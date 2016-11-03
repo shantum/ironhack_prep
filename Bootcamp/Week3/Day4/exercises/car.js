de1e13d3e1d334f1dd8df4572f82e03241513dd2
shantum <shantumgupta@gmail.com>
2016-11-03 11:03:12 +0100

    week3 day4 update

---
+++ Bootcamp/Week3/Day4/exercises/car.js
@@ -0,0 +1,32 @@
+// function Car (model, noise) {
+//     this.model     = model;
+//     this.noise     = noise;}
+
+// Car.prototype = {
+//     wheels: 4,
+//     makeNoise: function () {
+//         console.log(this.noise)}}
+
+// var ford = new Car('Model T', 'Vrooooom!');
+
+// ford.makeNoise();
+
+function Animal(age) {
+    this.age = age;
+}
+
+Animal.prototype.sayAge = function() {
+    console.log("Hi, my age is " + this.age)
+}
+
+function Dog(age) {
+    Animal.call(this, age); //call is a function that calls another function
+}
+
+Dog.prototype = new Animal(); //needs to be added before instantiation!
+
+var dog = new Dog('10 years');
+
+console.log(dog);
+
+dog.sayAge();
