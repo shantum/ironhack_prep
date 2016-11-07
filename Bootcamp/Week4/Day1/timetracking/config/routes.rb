bc381ceff902005acff46a14091fa33b8d8b3dff
shantum <shantumgupta@gmail.com>
2016-11-07 12:04:02 +0100

    updated

--- Bootcamp/Week4/Day1/timetracking/config/routes.rb
+++ Bootcamp/Week4/Day1/timetracking/config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'projects/index'
-
+  get 'projects/index
   get 'projects', to: 'projects#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
