cd5cda1bdbbbbf233f1257cf4d26adc10fa02658
shantum <shantumgupta@gmail.com>
2016-11-21 13:44:47 +0100

    added video game exercise files

--- Bootcamp/Week6/day1/rails api/Sandwichr/db/schema.rb
+++ Bootcamp/Week6/day1/rails api/Sandwichr/db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161121112803) do
+ActiveRecord::Schema.define(version: 20161121120147) do
 
   create_table "ingredients", force: :cascade do |t|
     t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "movies", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
