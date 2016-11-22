d6aae3d2369737d029e06e87b15be18783b1222b
shantum <shantumgupta@gmail.com>
2016-11-22 16:15:42 +0100

    added day 2 files

--- Project/data-gov-in/gov/app/controllers/datasets_controller.rb
+++ Project/data-gov-in/gov/app/controllers/datasets_controller.rb
@@ -1,2 +1,7 @@
 class DatasetsController < ApplicationController
+  def index
+    @dataset = Dataset.new
+    @url = "https://data.gov.in/api/datastore/resource.json?resource_id=df7798bb-2c7b-4ca9-932c-65bf01af8785&api-key=628ada9382aeb42471a46ee06ebc72b9"
+    @data = HTTP.get(@url)
+  end
 end
